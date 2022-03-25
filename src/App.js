3a8ea5a50f87d8ccf52a814640689f2d3f360239
John Beresford <jberesford@volcaus.com>
2022-03-24 18:02:40 -0700

    test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Gui from './components/gui';
 import Cursor from './components/Cursor';
 import { Suspense } from 'react';
 import { Helmet } from 'react-helmet';
+import favicon from './img/favicon.ico';
 
 function App() {
   const view = useStore((state) => state.view);
@@ -12,38 +13,14 @@ function App() {
   return (
     <div className='App'>
       <Helmet>
+        <link rel='icon' href='/favicon.ico' />
         {/* Google / Search Engine Tags */}
         <meta itemprop='name' content='John Beresford - Creative Developer' />
         <meta
           itemprop='description'
-          content='John Beresford is a multi-disciplinary designer and developer with a focus on 3D, computer graphics and web technologies.'
+          content='John M. Beresford is a multi-disciplinary designer and developer with a focus on 3D, computer graphics and web technologies.'
         />
         <meta itemprop='image' content='https://i.imgur.com/h09DAlo.jpg' />
-
-        {/* Facebook Meta Tags */}
-        <meta property='og:url' content='https://www.john-beresford.com' />
-        <meta property='og:type' content='website' />
-        <meta
-          property='og:title'
-          content='John Beresford - Creative Developer'
-        />
-        <meta
-          property='og:description'
-          content='John Beresford is a multi-disciplinary designer and developer with a focus on 3D, computer graphics and web technologies.'
-        />
-        <meta property='og:image' content='https://i.imgur.com/h09DAlo.jpg' />
-
-        {/* Twitter Meta Tags */}
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta
-          name='twitter:title'
-          content='John Beresford - Creative Developer'
-        />
-        <meta
-          name='twitter:description'
-          content='John Beresford is a multi-disciplinary designer and developer with a focus on 3D, computer graphics and web technologies.'
-        />
-        <meta name='twitter:image' content='https://i.imgur.com/h09DAlo.jpg' />
       </Helmet>
       {view === 'start' && <Loading />}
       <Gui />
