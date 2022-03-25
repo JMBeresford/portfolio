142364cb54b4aacc7d9d025dbc239ceb1e67ac88
John Beresford <jberesford@volcaus.com>
2022-03-24 18:03:03 -0700

    etc

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Gui from './components/gui';
 import Cursor from './components/Cursor';
 import { Suspense } from 'react';
 import { Helmet } from 'react-helmet';
-import favicon from './img/favicon.ico';
 
 function App() {
   const view = useStore((state) => state.view);
@@ -13,7 +12,6 @@ function App() {
   return (
     <div className='App'>
       <Helmet>
-        <link rel='icon' href='/favicon.ico' />
         {/* Google / Search Engine Tags */}
         <meta itemprop='name' content='John Beresford - Creative Developer' />
         <meta
