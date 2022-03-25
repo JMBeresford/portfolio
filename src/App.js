3c4781dbb4ffefd8d91135227e0c7d7e2b4a8fe8
John Beresford <jberesford@volcaus.com>
2022-03-24 17:35:28 -0700

    added react helmet :tada:

--- src/App.js
+++ src/App.js
@@ -4,12 +4,57 @@ import useStore from './store';
 import Gui from './components/gui';
 import Cursor from './components/Cursor';
 import { Suspense } from 'react';
+import { Helmet } from 'react-helmet';
+import banner from './img/banner.jpg';
 
 function App() {
   const view = useStore((state) => state.view);
 
   return (
     <div className='App'>
+      <Helmet>
+        {/* Google / Search Engine Tags */}
+        <meta itemprop='name' content='John Beresford - Creative Developer' />
+        <meta
+          itemprop='description'
+          content='John Beresford is a multi-disciplinary designer and developer with a focus on 3D, computer graphics and web technologies.'
+        />
+        <meta
+          itemprop='image'
+          content={`https://john-beresford.com${banner}`}
+        />
+
+        {/* Facebook Meta Tags */}
+        <meta property='og:url' content='https://www.john-beresford.com' />
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:title'
+          content='John Beresford - Creative Developer'
+        />
+        <meta
+          property='og:description'
+          content='John Beresford is a multi-disciplinary designer and developer with a focus on 3D, computer graphics and web technologies.'
+        />
+        <meta
+          property='og:image'
+          content={`https://john-beresford.com${banner}`}
+        />
+
+        {/* Twitter Meta Tags */}
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta
+          name='twitter:title'
+          content='John Beresford - Creative Developer'
+        />
+        <meta
+          name='twitter:description'
+          content='John Beresford is a multi-disciplinary designer and developer with a focus on 3D, computer graphics and web technologies.'
+        />
+        <meta
+          name='twitter:image'
+          content={`https://john-beresford.com${banner}`}
+        />
+      </Helmet>
       {view === 'start' && <Loading />}
       <Gui />
       <Suspense fallback={null}>
